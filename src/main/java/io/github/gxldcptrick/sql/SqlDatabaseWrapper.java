fa9404b15d1a4c3c0110af26c76b5563a1bfccb6
ANDRES H CARRERA <andreshcar@live.com>
2019-07-18 09:30:04 -0600

    basic shti

---
+++ src/main/java/io/github/gxldcptrick/sql/SqlDatabaseWrapper.java
@@ -0,0 +1,18 @@
+package io.github.gxldcptrick.sql;
+
+import io.github.gxldcptrick.security.LoginInfo;
+import io.github.gxldcptrick.sql.meta.ObjectId;
+
+import java.sql.Connection;
+import java.util.Map;
+
+public class SqlDatabaseWrapper {
+    private Map<String, SqlTableWrapper<?>> tables;
+    private Connection connection;
+    public SqlDatabaseWrapper(String connectionString, LoginInfo dbLogin){
+
+    }
+    public void addTable(String tableName, Class<? extends ObjectId> type){
+
+    }
+}
--- src/main/java/io/github/gxldcptrick/sql/meta/ReferenceTo.java
+++ src/main/java/io/github/gxldcptrick/sql/meta/ReferenceTo.java
@@ -9,4 +9,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ReferenceTo {
     Class<? extends ObjectId> TypeRefrenced();
+    String TableName();
 }
