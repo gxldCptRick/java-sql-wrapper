7b596b0e027bda9f121a193c17fabae27568cf1c
ANDRES H CARRERA <andreshcar@live.com>
2019-07-21 18:08:47 -0600

    removing the redundant id from the add call

--- src/main/java/io/github/gxldcptrick/data/access/DataStore.java
+++ src/main/java/io/github/gxldcptrick/data/access/DataStore.java
@@ -1,7 +1,7 @@
 package io.github.gxldcptrick.data.access;
 
 public interface DataStore<T>  extends ReadOnlyDataStore<T> {
-    void addRecord(int id, T data);
+    void addRecord(T data);
     void removeRecord(int id, T data);
     void updateRecord(int id, T data);
 }
